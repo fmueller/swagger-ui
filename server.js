a6478ecdad628693f2de0e5f367ff807c984d7c0
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-05-21 15:00:55 +0200

    get initial access token

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     server = express(),
     fs = require('fs'),
+    path = require('path'),
     index = fs.readFileSync('./index.html');
 server.use('/dist', express.static('dist'));
 
@@ -12,6 +13,11 @@ function generateEnv() {
                 env += '' + key + '="' + process.env[key] + '";\n';
             }
         }
+    }
+    // read client configuration from mint
+    if (process.env.CREDENTIALS_DIR) {
+        var clientConfig = JSON.parse(fs.readFileSync(path.join(process.env.CREDENTIALS_DIR, 'client.json')));
+        env += 'SUIENV_OAUTH_CLIENT_ID=' + clientConfig.client_id;
     }
     return env;
 }
