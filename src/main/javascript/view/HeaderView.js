2390db66f50bec032ba2ac342c85eadb89da1c89
Felix Müller <felix.mueller2@zalando.de>
2016-04-22 16:23:35 +0200

    changed loading of api definitions

--- src/main/javascript/view/HeaderView.js
+++ src/main/javascript/view/HeaderView.js
@@ -23,47 +23,9 @@ SwaggerUi.Views.HeaderView = Backbone.View.extend({
   },
 
   loadDefinition: function(e) {
-    var app_id = $(e.target).children(':selected').val(),
-        self = this;
-
-    function setOAuthHeader(xhr) {
-      var bearerHeader = 'Bearer ' + window.OAuthProvider.getAccessToken();
-      if (window.swaggerUi && window.swaggerUi.api) {
-        window.swaggerUi.api.clientAuthorizations.add('myoauth2',
-            new SwaggerClient.ApiKeyAuthorization('Authorization', bearerHeader, 'header'));
-      }
-      xhr.setRequestHeader('Authorization', bearerHeader);
-    }
-
-    $.ajax({
-        url: window.SUIENV_TWINTIP_BASE_URL + '/apps/' + app_id,
-        type: 'GET',
-        dataType: 'json',
-        beforeSend: setOAuthHeader,
-        error: console.error.bind(console),
-        success: function(api) {
-          var swaggerUrl = api.url,
-              isRelativeUrl = swaggerUrl[0] === '/';
-          if (isRelativeUrl) {
-            // go to kio for serviceurl
-            $.ajax({
-              url: window.SUIENV_KIO_BASE_URL + '/apps/' + app_id,
-              type: 'GET',
-              dataType: 'json',
-              beforeSend: setOAuthHeader,
-              error: console.error.bind(console),
-              success: function(app) {
-                self.trigger('update-swagger-ui', {
-                  url: app.service_url + swaggerUrl
-                });
-              }
-            });
-          } else {
-            self.trigger('update-swagger-ui', {
-              url: swaggerUrl
-            });
-          }
-      }
+    var app_id = $(e.target).children(':selected').val();
+    this.trigger('update-swagger-ui', {
+      url: window.SUIENV_TWINTIP_BASE_URL + '/apps/' + app_id + '/definition'
     });
   },
 
